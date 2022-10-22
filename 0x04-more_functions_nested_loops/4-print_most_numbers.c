ba68ac20369af155c2183e53c774603304beb9a8
ADVANCE2019 <solopat2016@gmail.com>
2022-10-21 21:13:50 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,19 @@
 * Return: 0 
 */ 
 void print_most_numbers(void)
-{ 
-	int num; 	    
-	   for (num = 0; num < 10; num++) 			            { 
-	   if ((num == 2) || (num == 4)) 
-           { 
-           continue; 
-						                        } 
-	   else 
-           { 
-						                                _putchar(num + '0'); 
-							                 } 
-    } 
-	    _putchar(10);
-}
+{
+	{ 
+	         int num;
+
+		 for (num = 0; num < 10; num++)}
+	         if ((num == 2) || (num == 4))
+		 {
+		 continue;
+		 }
+	         else 
+		 {
+		 _putchar(num + '0'); 
+		 } 
+	         } 
+	          _putchar(10);
+} 
