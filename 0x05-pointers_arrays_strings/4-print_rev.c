910022c643201840e7402eb378cfff79a22a1a6b
ADVANCE2019 <solopat2016@gmail.com>
2022-10-24 17:30:07 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,21 @@
-#include"main.h" 
-
-/** 
-* print_rev - prints a string in reverse 
-* @s: string parameter input 
-* Return: Nothing 
-*/ 
-  
-void print_rev(char *s) 
+#include "main.h"
+/**
+* print_rev - prints a string, in reverse, followed by a new line
+* @s: input string.
+* Return: no return.
+*/
+void print_rev(char *s)
 {
-      int index; 
-		    
-	/*finds the length of string without null character*/ 
-		for (index = 0; s[index] != '\0'; ++index) 
-                           ; 
-		     
-	 /*print char from the last index as you decrement*/ 
-			for (--index; index >= 0; --index) 
-		  _putchar(s[index]); 
-		  _putchar('\n'); 
+	int count = 0;
+
+	while (count >= 0)
+	{
+	if (s[count] == '\0')
+	break;
+		count++;
+		}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
