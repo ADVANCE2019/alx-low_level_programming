f1d0109ba9d7457b8f7f9467f0163d384f907047
ADVANCE2019 <solopat2016@gmail.com>
2022-10-27 17:29:12 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,12 +19,14 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if ((*(s + (n + 1)) >= 97) && (*(s + (n + 1)) <= 122))
-				*(s + (n + 1)) = *(s + (n + 1)) - 32;
-			break;
+			if (*(s + n) == cap_words[i])
+			{
+				if ((*(s + (n + 1)) >= 97) && (*(s + (n + 1)) <= 122))
+					*(s + (n + 1)) = *(s + (n + 1)) - 32;
+				break;
+			}
 		}
+		n++;
 	}
-	n++;
-}
-return (s);
+	return (s);
 }
